1479fc6c0df1537002a8680b3ac98e0e67538e63
Woody Rousseau <woody.a.rousseau@gmail.com>
2016-01-18 21:42:43 +0100

    Optimizes settings save

--- client/scripts/settings/controllers/settings.js
+++ client/scripts/settings/controllers/settings.js
@@ -78,44 +78,39 @@ module.exports = function(app) {
       if (!$scope.isProcessingRequest) {
         $scope.isProcessingRequest = true;
         var customerId = Credentials.get().id;
-
-        // TODO : make request only if changes have been made
         Network.hasConnectivity()
         .then(function() {
           return ElementModifier.validate($scope.form.customerEdit);
         })
         .then(function() {
-          return Customer.update(customerId, $scope.customerIdentity);
+          var authenticationParams = _.pick($scope.customerIdentity, ['email', 'oldPassword', 'newPassword']);
+          var promises = [
+            Customer.update(customerId, $scope.customerIdentity),
+            Authentication.updatePassword(authenticationParams),
+            CustomerSettings.update(customerId, $scope.customerSettings)
+          ];
+          var addressParams = Address.getAddressFromResidencyInformation($scope.customerAddress.residency);
+          if (addressParams)
+          {
+            addressParams = _.merge(addressParams, {details: $scope.customerAddress.details});
+            promises.push(Address.update(customerId, addressParams));
+          }
+          return $q.all(promises);
         })
-        .then(function(customer) {
+        .then(function(data) {
+          $scope.oldPassword = '';
+          $scope.newPassword = '';
+          var customer = data[0];
           customer.phoneNumber = PhoneFormat.formatPhoneNumberForFrontend(customer.phoneNumber);
           $scope.customerIdentity = customer;
           IonicUser.set(_.omit(customer, ['activated']));
           CustomerStorage.setIdentity(customer);
-          var addressParams = Address.getAddressFromResidencyInformation($scope.customerAddress.residency);
-          if (!addressParams)
-          {
-            // If no residency was provided, not requesting the Address update
-            return $q.when({});
+          IonicUser.set(data[2]);
+          CustomerStorage.setSettings(data[2]);
+          if (data.length === 4) {
+            IonicUser.set(data[3]);
+            CustomerStorage.setAddress(data[3]);
           }
-          addressParams = _.merge(addressParams, {details: $scope.customerAddress.details});
-          return Address.update(customerId, addressParams);
-        })
-        .then(function(address) {
-          IonicUser.set(address);
-          CustomerStorage.setAddress(address);
-          var authenticationParams = _.pick($scope.customerIdentity, ['email', 'oldPassword', 'newPassword']);
-          return Authentication.updatePassword(authenticationParams);
-        })
-        .then(function() {
-          // Clearing both passwords
-          $scope.oldPassword = '';
-          $scope.newPassword = '';
-          return CustomerSettings.update(customerId, $scope.customerSettings);
-        })
-        .then(function(customerSettings) {
-          IonicUser.set(customerSettings);
-          CustomerStorage.setSettings(customerSettings);
           $scope.isProcessingRequest = false;
           return Popup.title('customerEdited');
         })
