10d2f9c7bd432937febc30c606c6c68741cbe413
Woody Rousseau <woody.a.rousseau@gmail.com>
2016-02-20 20:15:03 +0100

    Fixes hook for adding notification icon

--- client/cordova/app/hooks/add_android_resources.js
+++ client/cordova/app/hooks/add_android_resources.js
@@ -2,25 +2,58 @@
 
 // each object in the array consists of a key which refers to the source and
 // the value which is the destination.
-var filestocopy = [{
-    "resources/android/notification.png":
-    "platforms/android/res/drawable/notification.png"
-}];
+
 
 var fs = require('fs');
 var path = require('path');
 
-var rootdir = process.argv[2];
+module.exports = function(ctx) {
 
-filestocopy.forEach(function(obj) {
-    Object.keys(obj).forEach(function(key) {
-        var val = obj[key];
-        var srcfile = path.join(rootdir, key);
-        var destfile = path.join(rootdir, val);
-        var destdir = path.dirname(destfile);
-        if (fs.existsSync(srcfile) && fs.existsSync(destdir)) {
-            fs.createReadStream(srcfile).pipe(
-               fs.createWriteStream(destfile));
-        }
-    });
-});
+  var exportFolders = [
+    'drawable-hdpi',
+    'drawable-land-hdpi',
+    'drawable-land-ldpi',
+    'drawable-land-mdpi',
+    'drawable-land-xhdpi',
+    'drawable-ldpi',
+    'drawable-mdpi',
+    'drawable-port-hdpi',
+    'drawable-port-ldpi',
+    'drawable-port-mdpi',
+    'drawable-port-xhdpi',
+    'drawable-xhdpi'
+  ]
+
+  var filesToCopy = [];
+
+  for (var i = 0; i < exportFolders.length; i++) {
+    var fileToCopy = {};
+    fileToCopy['resources/android/notification.png'] = 'platforms/android/res/' + exportFolders[i] + '/notification.png';
+    filesToCopy.push(fileToCopy);
+  }
+
+  console.log(filesToCopy);
+
+  var deferral = ctx.requireCordovaModule('q').defer();
+
+  var rootDir = ctx.opts.projectRoot;
+
+  filesToCopy.forEach(function(obj) {
+      Object.keys(obj).forEach(function(key) {
+        process.stdout.write(key);
+
+          var val = obj[key];
+          process.stdout.write(val);
+
+          var srcFile = path.join(rootDir, key);
+          var destFile = path.join(rootDir, val);
+          var destDir = path.dirname(destFile);
+          if (fs.existsSync(srcFile) && fs.existsSync(destDir)) {
+              fs.createReadStream(srcFile).pipe(
+                 fs.createWriteStream(destFile));
+          }
+      });
+  });
+
+  return deferral.promise;
+}
